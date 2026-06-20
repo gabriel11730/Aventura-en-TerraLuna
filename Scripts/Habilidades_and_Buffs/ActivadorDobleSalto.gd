035de6b21757f7e4559bea6ef9ba3415a42fffde
Jesus Gabriel Garcia Marquez <jesusgabriel_30@hotmail.com>
2026-06-20 00:45:30 -0400

    fix: Soluciones menores

--- Scripts/Habilidades_and_Buffs/ActivadorDobleSalto.gd
+++ Scripts/Habilidades_and_Buffs/ActivadorDobleSalto.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var activador_conseguido: AudioStreamPlayer2D = $ActivadorConseguido
+@onready var area_2d: Area2D = $Area2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Personaje"):
+		area_2d.set_deferred("monitoring", false)
 		GameState.desbloquear_doble_salto()
 		activador_conseguido.play()
 		visible = false
--- Scripts/Habilidades_and_Buffs/ActivadorSword.gd
+++ Scripts/Habilidades_and_Buffs/ActivadorSword.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var activador_conseguido: AudioStreamPlayer2D = $ActivadorConseguido
+@onready var area_2d: Area2D = $Area2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Personaje"):
+		area_2d.set_deferred("monitoring", false)
 		GameState.desbloquear_sword()
 		body.set_animations(true)
 		activador_conseguido.play()
